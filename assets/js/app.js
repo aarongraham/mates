2fb23e3d71a12304fa706a8946c9bacc4237023c
Aaron Graham <agraham19@gmail.com>
2019-12-29 16:16:29 -0800

    Add prettier and format js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from '../css/app.css'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,16 +9,16 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
-import {Socket} from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import 'phoenix_html'
+import { Socket } from 'phoenix'
+import LiveSocket from 'phoenix_live_view'
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-import QRScanner from "./qr_scanner"
+import QRScanner from './qr_scanner'
 window.QRScanner = QRScanner
 
-let liveSocket = new LiveSocket("/live", Socket)
-liveSocket.connect();
+let liveSocket = new LiveSocket('/live', Socket)
+liveSocket.connect()
