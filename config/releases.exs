b19d9353f3da756fe44a2934750ff9a0ad4157fa
Aaron Graham <agraham19@gmail.com>
2020-01-16 17:00:38 -0800

    There is no get_env!

--- config/releases.exs
+++ config/releases.exs
@@ -1,10 +1,10 @@
 import Config
 
-secret_key_base = System.get_env!("SECRET_KEY_BASE")
-canonical_host = System.get_env!("CANONICAL_HOST")
+secret_key_base = System.get_env("SECRET_KEY_BASE")
+canonical_host = System.get_env("CANONICAL_HOST")
 
 {type, encoded, _atom} =
-  System.get_env!("SECRET_KEY") |> Base.decode64!() |> :public_key.pem_decode() |> hd()
+  System.get_env("SECRET_KEY") |> Base.decode64!() |> :public_key.pem_decode() |> hd()
 
 config :mates, MatesWeb.Endpoint,
   url: [
